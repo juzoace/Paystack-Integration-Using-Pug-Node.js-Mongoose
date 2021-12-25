79bc45aa5303a8d0338e767ae9cd1e0219e51675
Nwigwe Uzochukwu <uzochukwunwigwe@gmail.com>
2021-12-25 21:38:30 +0100

    Setup server and index pug file

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+const pug = require('pug');
+const _ = require('lodash');
+const path = require('path');
+const app = express
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public/')));
+app.set('view engine', pug);
+
+app.get('/',(req, res) => {
+    res.render('index.pug');
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`)
+});
